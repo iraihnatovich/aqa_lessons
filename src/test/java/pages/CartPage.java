c79afac54455f4eb8c4852aabfc4cd7ecf166ba5
iihnatovich <i.ihnatovich@gmail.com>
2024-03-23 22:21:04 +0300

    PageObject

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 
 public class CartPage extends BasePage {
-    //    private final By cartItemLocator = By.cssSelector("#cart_item_label");
-//    private final By cartBadgeLocator = By.cssSelector("#shopping_cart_badge");
     private final By checkoutButtonLocator = By.cssSelector("#checkout");
 
     public CartPage(WebDriver driver) {
@@ -20,19 +18,6 @@ public class CartPage extends BasePage {
     protected By getPageIdentifier() {
         return checkoutButtonLocator;
     }
-//    public List <WebElement> getCartItems(){
-//        return wait.waitForAllVisibleElementsLocatedBy(cartItemLocator);
-//    }
-//
-//    public WebElement getCartSingleItem(int index){
-//        return getCartItems().get(index);
-//    }
-//    public WebElement getCartBadge(){
-//        return wait.waitForVisibilityLocatedBy(cartBadgeLocator);
-//    }
-//    public int getAmountOfCartItems(){
-//        return Integer.parseInt(getCartBadge().getText()) ;
-//    }
 
     public WebElement getCheckoutButton() {
         return wait.waitForVisibilityLocatedBy(checkoutButtonLocator);
